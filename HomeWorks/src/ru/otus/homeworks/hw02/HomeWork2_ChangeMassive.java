a4c5908f32f166f10a3bafea4d293c7144ec3cde
Artem <sarkisyan_aa@interrao.ru>
2022-12-04 00:28:32 +0300

    update HomeWork2_ChangeMassive

--- HomeWorks/src/ru/otus/homeworks/hw02/HomeWork2_ChangeMassive.java
+++ HomeWorks/src/ru/otus/homeworks/hw02/HomeWork2_ChangeMassive.java
@@ -1,21 +1,14 @@
 package ru.otus.homeworks.hw02;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
 
 public class HomeWork2_ChangeMassive {
-    private static String[] append(String[] arr, String newElement) {
-        List<String> list = new ArrayList<>(Arrays.asList(arr));
-        list.add(newElement);
-
-        return list.toArray(new String[0]);
-    }
 
     public static void main(String[] args) {
         String[] mas1 = {"test"};
-
-        mas1 = append(mas1, "НОВОЕ ЗНАЧЕНИЕ");
+        mas1 = Arrays.copyOf(mas1, mas1.length + 1);
+        mas1[1] = "НОВОЕ ЗНАЧЕНИЕ";
         System.out.println(Arrays.toString(mas1));
     }
 }
