0aaa28dbd3a3870331e6b2404f1d7e5024aeefaa
Artem <sarkisyan_aa@interrao.ru>
2023-01-17 20:17:41 +0300

    update HomeWork3

--- HomeWorks/src/ru/otus/homeworks/hw03/HomeWork3.java
+++ HomeWorks/src/ru/otus/homeworks/hw03/HomeWork3.java
@@ -1,31 +1,28 @@
 package ru.otus.homeworks.hw03;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Questions {
-    public String question;
-    public int trueAnswer;
+    private String question;
+    private int trueAnswer;
 
-    public int[] questionAnswers;
+    private int[] questionAnswers;
 
-    public boolean checkAnswer() {
-        System.out.println(question);
-        for (int n = 0; n < questionAnswers.length; n++) {
-            System.out.println(n + 1 + ". " + questionAnswers[n]);
-        }
-        System.out.print("Выберите вариант ответа: ");
-        Scanner scanner = new Scanner(System.in);
-        int answer = scanner.nextInt();
-        if (answer == trueAnswer) {
-            System.out.println("Правильно");
-            return true;
-        } else {
-            System.out.println("Неправильно");
-            return false;
 
-        }
+    public String getQuestion() {
+        return question;
     }
+
+    public int getTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public int[] getQuestionAnswers() {
+        return Arrays.copyOf(questionAnswers, questionAnswers.length);
+    }
+
 
     public Questions(String question, int trueAnswer, int[] questionAnswers) {
         this.question = question;
@@ -42,21 +39,31 @@ public class HomeWork3 {
         Questions question1 = new Questions("Сколько будет 1+1?", 1, new int[]{2, 3, 4, 10, 11});
         Questions question2 = new Questions("Сколько будет 2+2?", 1, new int[]{4, 5, 6, 10});
         Questions question3 = new Questions("Сколько будет 3+3?", 2, new int[]{5, 6, 7});
-        if (question1.checkAnswer()) {
-            correctCount++;
-        } else {
-            wrongCount++;
+
+
+        Questions[] questionsAnswers = {question1, question2, question3};
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(questionsAnswers[i].getQuestion());
+            for (int n = 0; n < questionsAnswers[i].getQuestionAnswers().length; n++) {
+                System.out.println(n + 1 + ". " + questionsAnswers[i].getQuestionAnswers()[n]);
+            }
+            System.out.print("Выберите вариант ответа: ");
+            Scanner scanner = new Scanner(System.in);
+            int answer = scanner.nextInt();
+            if (answer == questionsAnswers[i].getTrueAnswer()) {
+                System.out.println("Правильно");
+                correctCount++;
+            } else {
+                System.out.println("Неправильно");
+                wrongCount++;
+            }
+            System.out.println();
         }
-        if (question2.checkAnswer()) {
-            correctCount++;
-        } else {
-            wrongCount++;
-        }
-        if (question3.checkAnswer()) {
-            correctCount++;
-        } else {
-            wrongCount++;
-        }
+
         System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
     }
+
+
 }
